26230ef815033ccdf7ac3b55c4b73ea2f850912c
zachpfaltzgraff <zachpfaltzgraff@gmail.com>
2024-03-06 11:58:30 -0500

    mapped layout of gameLogic

--- src/main/java/com/example/monopolyjavafx/GameBoard.java
+++ src/main/java/com/example/monopolyjavafx/GameBoard.java
@@ -3,7 +3,22 @@ package com.example.monopolyjavafx;
 public class GameBoard {
     private String[][] GameGrid = new String[11][11];
 
+    /**
+     * gameLogic
+     * 0 = Property Squares
+     * 9 = Property Square owned
+     * 1 = RailRoad Squares
+     * 2 = Utility / Tax Squares
+     * 3 = Community Chest
+     * 4 = Chance Square
+     * 5 = Go To Jail
+     * 6 = Jail / Just Visiting
+     * 7 = Free Parking
+     * 8 = Go / Start
+     */
+    private int[][] gameLogic = new int[11][11];
+
     GameBoard() {
-        GameGrid[0][0] = "";
+        GameGrid[0][0] = "Start";
     }
 }
--- src/main/java/com/example/monopolyjavafx/Monopoly.java
+++ src/main/java/com/example/monopolyjavafx/Monopoly.java
@@ -1,14 +1,13 @@
 package com.example.monopolyjavafx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Monopoly extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         GridPane gameGrid = new GridPane();
