30f4ea776f8b64edaaeffb5a68a82873c1db93f9
zachpfaltzgraff <zachpfaltzgraff@gmail.com>
2024-03-06 11:48:06 -0500

    created project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.monopolyjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.monopolyjavafx to javafx.fxml;
+    exports com.example.monopolyjavafx;
+}
