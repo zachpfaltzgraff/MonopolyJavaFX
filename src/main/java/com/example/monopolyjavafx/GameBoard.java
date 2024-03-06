85a224fea59464979550236caf7e59f122db338a
zachpfaltzgraff <zachpfaltzgraff@gmail.com>
2024-03-06 11:52:48 -0500

    GameBoard created
    created class for GameBoard, will hold the logic of the board

---
+++ src/main/java/com/example/monopolyjavafx/GameBoard.java
@@ -0,0 +1,9 @@
+package com.example.monopolyjavafx;
+
+public class GameBoard {
+    private String[][] GameGrid = new String[11][11];
+
+    GameBoard() {
+        GameGrid[0][0] = "";
+    }
+}
--- src/main/java/com/example/monopolyjavafx/HelloApplication.java
+++ src/main/java/com/example/monopolyjavafx/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.monopolyjavafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        GridPane gameGrid = new GridPane();
+        Scene scene = new Scene(gameGrid, 1080, 720);
+        stage.setTitle("Monopoly");
         stage.setScene(scene);
         stage.show();
     }
